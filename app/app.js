92f668f4ca4e22a493e1d9145f91bfac841954ea
SamGinzburg <ginzburg.sam@gmail.com>
2016-11-11 22:13:26 -0500

    Enabled react for all the pages, converted search_results to react format

--- app/app.js
+++ app/app.js
@@ -1,1 +1,6 @@
-/* Empty for now. :) */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchResults from './components/SearchResults'
+
+
+ReactDOM.render(<SearchResults />, document.getElementById("search-results"));
---
+++ app/components/SearchResults.js
@@ -0,0 +1,118 @@
+import React from 'react';
+
+export default class SearchResults extends React.Component
+{
+  render()
+  {
+    return (
+      <div>
+      <div className="panel panel-default panel-colors">
+        <div className="panel-body">
+          <div className="row">
+            <div className="col-md-10 col-centered">
+              <ul className="nav nav-pills">
+                <li role="presentation">
+                  <img width="200" src="http://maps.googleapis.com/maps/api/staticmap?center=42.38666512+-72.52333124&zoom=13&scale=1&size=200x200&maptype=roadmap&key=AIzaSyDGuGiLXDNcw84JU6I7ldZF63sxBLP6z7s&format=png&visual_refresh=true&markers=size:mid%7Ccolor:0xff0000%7Clabel:1%7C42.389825, -72.528267" alt="Google Map of 42.389825, -72.528267"/>
+                </li>
+                <li className="pull-right">
+                  <a href="/profile.html" className="override-boostrap-hyperlink">OtherUser321</a>
+                  <br><text>June 1, 2016</text></br>
+                </li>
+                <li role="presentation" className="active">
+                  <div className="controls text-wrap">
+                    <p className="form-control-static post-text">
+                       Lorem ipsum dolor sit amet, ut sit, lobortis eros arcu et imperdiet elit, dolor rhoncus ipsum placerat. Leo eu. Eleifend metus, sem amet egestas diam lorem. Duis dui, mollis cras arcu eget dignissim. Phasellus quam className, gravida ut ut ligula accumsan odio. Eget congue. Ac nec tortor feugiat urna feugiat vivamus, leo interdum, integer neque at justo ac justo, eu duis augue nulla, in tincidunt curabitur eleifend ac. Mauris suspendisse, vel quisque mauris id quis nullam mi, ante quam quam lobortis sed vestibulum pede, risus ut in risus tincidunt at.
+Morbi at rerum. Pellentesque nunc euismod non id, malesuada euismod. Rerum sapien placerat potenti nunc commodo ullamcorper, lectus tempus fusce massa rutrum sit. Curabitur velit egestas id est, justo enim imperdiet, eget commodo tortor, integer venenatis sagittis, mollis dolor pellentesque. Quisque vehicula venenatis massa, pede in interdum metus, iaculis gravida quam ultrices euismod est, vestibulum ut ultricies nec consectetuer quis. Cras platea id vestibulum tellus nam euismod. Non dictum, diam pede lorem a, ultrices molestiae morbi sed. Purus pulvinar, molestie ut, eu hendrerit ac a dictum eleifend, consequat mattis in sed, arcu nec vestibulum sit sociosqu turpis. Amet quis amet enim dictum, erat nulla nulla amet erat eros placerat, mattis non nunc justo varius lectus a, sed massa sunt vestibulum, turpis lorem. Et et. Potenti massa, nec ut litora ipsum, enim ante nec semper ante duis. Sed eget blandit, mi vestibulum augue placerat vulputate sed, porttitor consectetuer sed mauris veritatis, eget dis accumsan sed pellentesque commodo imperdiet.
+Sem cras. Per ullamcorper in fusce duis varius porttitor, ut mi ante justo iaculis. Ligula quam, non quam id suspendisse ac wisi, curabitur justo nisl at nec sem, scelerisque aliquam nam, sem lacus erat donec vel. Quis sed at, turpis lectus, tempor mattis morbi luctus magna euismod nam, mauris tincidunt. In ante dignissim nullam natoque leo. Lectus accumsan nulla a pellentesque. Eu est fusce vel, quis sed ipsum senectus non eget, molestie sociis vestibulum fusce justo, rutrum ornare non. Turpis in mollitia dolor, mi etiam massa vulputate lobortis, imperdiet convallis ipsum optio sodales lorem et, faucibus adipiscing nunc ut mauris est ornare. Lacus mollis blandit ullamcorper eu amet. At aliquam mauris cras. Id integer ligula pellentesque eget vehicula nec.
+Euismod libero vivamus dignissim mi, aliquam neque malesuada suscipit, eu quisque aliquam quam quisque, cupiditate nam. Nam vitae amet urna suspendisse dolorum nisi, proin proin rhoncus aliquet tellus. Enim nam nullam molestias vestibulum suscipit. Viverra vitae leo nam. Ullamcorper ornare, tellus pretium tempus viverra sem, vestibulum vestibulum tempor tristique nibh, et orci at. Lectus lacus congue nec placerat dignissim ut. Sagittis nisl pharetra, proin ipsum feugiat ac iaculis aliquam porta, diam ut at. Nibh erat, in nec amet ut ac nunc erat, phasellus tempus metus, faucibus ante odio interdum convallis venenatis. Erat sagittis aliquam nam vestibulum venenatis porttitor, lobortis volutpat et, integer vestibulum aenean vivamus nisl est, sed suscipit, quam cras metus lacus at est pellentesque. Dictum cubilia cras, integer fusce justo, vel mi, nunc consectetuer ipsum enim inceptos nam erat. Nulla vel, erat ante leo ullamcorper diam fringilla, felis luctus mauris, donec mi fusce posuere sit rhoncus.
+                    </p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-md-10">
+
+              <ul className="nav nav-pills pull-left post-options">
+                <li className="col-centered">
+                  <span className="glyphicon glyphicon-menu-up upvote">127</span>
+                </li>
+                <li className="col-centered">
+                  <span className="glyphicon glyphicon-menu-down downvote">6790</span>
+                </li>
+              </ul>
+
+              <ul className="nav nav-pills pull-right post-options">
+                <li role="presentation" className="active">
+                  <a href="#"><span className="glyphicon glyphicon-pencil">
+                  </span> <strong>View Comments</strong></a>
+                </li>
+                <li role="presentation" className="active">
+                  <a href="#"><span className="glyphicon glyphicon-triangle-right">
+                  </span> <strong>View Full Post</strong></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="panel panel-default">
+        <div className="panel-body">
+          <div className="row">
+            <div className="col-md-10 col-centered">
+              <ul className="nav nav-pills">
+                <li role="presentation">
+                  <img width="200" src="http://maps.googleapis.com/maps/api/staticmap?center=42.3872217844+-72.5236179055&zoom=13&scale=1&size=200x200&maptype=roadmap&key=AIzaSyDGuGiLXDNcw84JU6I7ldZF63sxBLP6z7s&format=png&visual_refresh=true&markers=size:mid%7Ccolor:0xff0000%7Clabel:1%7C42.3809392,+-72.5204432" alt="Google Map of 42.38666512 -72.52333124"/>
+                </li>
+
+                <li className="pull-right">
+                  <a href="/profile.html" className="override-boostrap-hyperlink">CouldntThinkofaName1337</a>
+                  <br><text>September 1, 2016</text></br>
+                </li>
+                <li role="presentation" className="active">
+                  <div className="controls text-wrap">
+                    <p className="form-control-static post-text">
+                       Lorem ipsum dolor sit amet, ut sit, lobortis eros arcu et imperdiet elit, dolor rhoncus ipsum placerat. Leo eu. Eleifend metus, sem amet egestas diam lorem. Duis dui, mollis cras arcu eget dignissim. Phasellus quam className, gravida ut ut ligula accumsan odio. Eget congue. Ac nec tortor feugiat urna feugiat vivamus, leo interdum, integer neque at justo ac justo, eu duis augue nulla, in tincidunt curabitur eleifend ac. Mauris suspendisse, vel quisque mauris id quis nullam mi, ante quam quam lobortis sed vestibulum pede, risus ut in risus tincidunt at.
+  Morbi at rerum. Pellentesque nunc euismod non id, malesuada euismod. Rerum sapien placerat potenti nunc commodo ullamcorper, lectus tempus fusce massa rutrum sit. Curabitur velit egestas id est, justo enim imperdiet, eget commodo tortor, integer venenatis sagittis, mollis dolor pellentesque. Quisque vehicula venenatis massa, pede in interdum metus, iaculis gravida quam ultrices euismod est, vestibulum ut ultricies nec consectetuer quis. Cras platea id vestibulum tellus nam euismod. Non dictum, diam pede lorem a, ultrices molestiae morbi sed. Purus pulvinar, molestie ut, eu hendrerit ac a dictum eleifend, consequat mattis in sed, arcu nec vestibulum sit sociosqu turpis. Amet quis amet enim dictum, erat nulla nulla amet erat eros placerat, mattis non nunc justo varius lectus a, sed massa sunt vestibulum, turpis lorem. Et et. Potenti massa, nec ut litora ipsum, enim ante nec semper ante duis. Sed eget blandit, mi vestibulum augue placerat vulputate sed, porttitor consectetuer sed mauris veritatis, eget dis accumsan sed pellentesque commodo imperdiet.
+  Sem cras. Per ullamcorper in fusce duis varius porttitor, ut mi ante justo iaculis. Ligula quam, non quam id suspendisse ac wisi, curabitur justo nisl at nec sem, scelerisque aliquam nam, sem lacus erat donec vel. Quis sed at, turpis lectus, tempor mattis morbi luctus magna euismod nam, mauris tincidunt. In ante dignissim nullam natoque leo. Lectus accumsan nulla a pellentesque. Eu est fusce vel, quis sed ipsum senectus non eget, molestie sociis vestibulum fusce justo, rutrum ornare non. Turpis in mollitia dolor, mi etiam massa vulputate lobortis, imperdiet convallis ipsum optio sodales lorem et, faucibus adipiscing nunc ut mauris est ornare. Lacus mollis blandit ullamcorper eu amet. At aliquam mauris cras. Id integer ligula pellentesque eget vehicula nec.
+  Euismod libero vivamus dignissim mi, aliquam neque malesuada suscipit, eu quisque aliquam quam quisque, cupiditate nam. Nam vitae amet urna suspendisse dolorum nisi, proin proin rhoncus aliquet tellus. Enim nam nullam molestias vestibulum suscipit. Viverra vitae leo nam. Ullamcorper ornare, tellus pretium tempus viverra sem, vestibulum vestibulum tempor tristique nibh, et orci at. Lectus lacus congue nec placerat dignissim ut. Sagittis nisl pharetra, proin ipsum feugiat ac iaculis aliquam porta, diam ut at. Nibh erat, in nec amet ut ac nunc erat, phasellus tempus metus, faucibus ante odio interdum convallis venenatis. Erat sagittis aliquam nam vestibulum venenatis porttitor, lobortis volutpat et, integer vestibulum aenean vivamus nisl est, sed suscipit, quam cras metus lacus at est pellentesque. Dictum cubilia cras, integer fusce justo, vel mi, nunc consectetuer ipsum enim inceptos nam erat. Nulla vel, erat ante leo ullamcorper diam fringilla, felis luctus mauris, donec mi fusce posuere sit rhoncus.
+                    </p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-md-10">
+
+              <ul className="nav nav-pills pull-left post-options">
+                <li className="col-centered">
+                  <span className="glyphicon glyphicon-menu-up upvote">1023</span>
+                </li>
+                <li className="col-centered">
+                  <span className="glyphicon glyphicon-menu-down downvote">12</span>
+                </li>
+              </ul>
+
+
+              <ul className="nav nav-pills pull-right post-options">
+                <li role="presentation" className="active nav-btn-pills">
+                  <a href="#"><span className="glyphicon glyphicon-pencil">
+                  </span> <strong>View Comments</strong></a>
+                </li>
+                <li role="presentation" className="active">
+                  <a href="#"><span className="glyphicon glyphicon-triangle-right">
+                  </span> <strong>View Full Post</strong></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>)
+  }
+}
