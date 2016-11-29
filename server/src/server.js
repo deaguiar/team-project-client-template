d6cc1f9c121772fd3278ed67ac6e10d68aff1cef
Philip Walne <pwalne12@gmail.com>
2016-11-29 14:39:02 -0500

    added listener & moved dummy database over to server.

--- server/src/server.js
+++ server/src/server.js
@@ -1,2 +1,8 @@
 // Implement your server in this file.
+var express = require('express');
+var app = express();
 // We should be able to run your server with node src/server.js
+// Starts the server on port 3000!
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
