63006fa6d235b6fa038f81d9180345fd9c3eb716
Philip Walne <pwalne12@gmail.com>
2016-11-29 22:30:01 -0500

    added code from workshop6

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,44 @@
-// Implement your server in this file.
+// Imports
 var express = require('express');
 var app = express();
-// We should be able to run your server with node src/server.js
+var db = require('./database.js'); //Database.js import
+var bodyParser = require('body-parser');
+
+var port = 3000;
+
+//Express
+app.use(bodyParser.text());
+app.use(bodyParser.json());
+app.use(express.static("../client/build"));
+
+/**
+ * Get the user ID from a token. Returns -1 (an invalid ID)
+ * if it fails.
+ */
+function getUserIdFromToken(authorizationLine) {
+    try {
+        var token = authorizationLine.slice(7);
+        var regularString = new Buffer(token, 'base64').toString('utf8');
+        var tokenObj = JSON.parse(regularString);
+        var id = tokenObj['id'];
+        if (typeof id === 'number') {
+            return id;
+        } else {
+            return -1;
+        }
+    } catch (e) {
+        return -1;
+    }
+}
+
 // Starts the server on port 3000!
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+    console.log('Geopost listening on port: ' + port);
 });
+app.use(function(err, req, res, next) {
+    if (err.name === 'JsonSchemaValidation') {
+        res.status(400).end();
+    } else {
+        next(err);
+    }
+})
